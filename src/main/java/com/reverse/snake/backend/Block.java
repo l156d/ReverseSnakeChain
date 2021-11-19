b0d7dbc92019c2977f7019c8975832525746418d
Silvio Ciavarrella <ciavarrella@mail.hs-ulm.de>
2021-11-19 15:35:13 +0100

    Init Project

---
+++ src/main/java/com/reverse/snake/backend/Block.java
@@ -0,0 +1,12 @@
+package com.reverse.snake.backend;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Block {
+
+}
---
+++ src/test/java/com/reverse/snake/SnakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reverse.snake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
